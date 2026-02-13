0a2bb528a9b8e817c00f40aae53f8ea7ea920188
Anurag <anuragmenon94@gmail.com>
2026-02-14 01:18:48 +0530

    CORS FIX

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from contextlib import asynccontextmanager
 from app.rag import query_rag, build_vectordb_if_missing
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -17,6 +18,22 @@ app = FastAPI(lifespan=lifespan)
 
 class QueryRequest(BaseModel):
     question: str
+
+# Add this CORS middleware
+origins = [
+    "http://localhost:5173",  # your dev frontend
+    "https://rag-ui-production.up.railway.app/",  # production frontend
+    "https://rag-ui-production.up.railway.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,       # allow your frontend domain
+    allow_credentials=True,
+    allow_methods=["*"],         # allow POST, GET, OPTIONS, etc.
+    allow_headers=["*"]          # allow Content-Type, Authorization, etc.
+)
+
 
 
 @app.get("/")
